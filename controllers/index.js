0cd3bd006461b90cf514fbe9ee72afe93db4346b
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-04-12 16:47:45 +0300

    added transactions routes and controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const authCtrls = require("./auth");
 const usersCtrls = require("./users");
+const transactionsCtrls = require("./transactions");
 
 module.exports = {
   authCtrls,
   usersCtrls,
+  transactionsCtrls,
 };
---
+++ controllers/transactions/index.js
@@ -0,0 +1,7 @@
+const getTransactions = require("./getTransactions");
+const addTransaction = require("./addTransaction");
+
+module.exports = {
+  getTransactions,
+  addTransaction,
+};
