9fe8e8eb7ccd21c0bb46d049b75ceeb18e93548a
Ivan Roschin <ivan.roschin86@gmail.com>
2023-04-12 17:38:46 +0300

    add delete user

--- controllers/users/getCurrent.js
+++ controllers/users/getCurrent.js
@@ -1,10 +1,10 @@
-const { Transactions } = require("../../models");
+const { Transaction } = require("../../models");
 // const { User } = require("../../models");
 
 const getCurrent = async (req, res) => {
   const { _id, name, email, phone, balance, image } = req.user;
 
-  const userTransactions = await Transactions.find(
+  const userTransactions = await Transaction.find(
     { owner: _id },
     "-createdAt -updatedAt"
   ).populate("owner", "_id date, type, category, comment, sum");
