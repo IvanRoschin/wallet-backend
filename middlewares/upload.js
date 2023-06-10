407316210659fb91ae3591fe4204fa74807c759e
Ivan Roschin <ivan.roschin86@gmail.com>
2023-06-10 12:52:04 +0300

    upload folder

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -6,6 +6,8 @@ const tmpDir = path.resolve("tmp");
 const multerConfig = multer.diskStorage({
   destination: (req, file, cb) => {
     console.log("file", file);
+    console.log("tmpDir", file);
+
     cb(null, tmpDir);
   },
   filename: (req, file, cb) => {
